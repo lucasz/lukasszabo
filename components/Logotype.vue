890bda2f3efb15879a9c13c9eacf9206d8f760bf
Lukáš Szabó <ls_freelance@hotmail.com>
2023-02-27 08:45:19 +0100

    este jeden

--- components/Logotype.vue
+++ components/Logotype.vue
@@ -1,3 +1,3 @@
 <template >
-    <img src="~/assets/img/photo.png" alt="" style="height: 200px; max-width: 200px; margin-top: -100px; margin-left: -50px;"/>
+    <NuxtLink href="/"><img src="~/assets/img/photo.png" alt="" style="height: 200px; max-width: 200px; margin-top: -100px; margin-left: -50px;"/></NuxtLink>
 </template>
