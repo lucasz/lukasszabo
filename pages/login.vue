2c59a87537dc38771e02ce630171275c59b40032
Lukáš Szabó <ls_freelance@hotmail.com>
2023-03-01 19:33:48 +0100

    verzia je verzia

---
+++ pages/login.vue
@@ -0,0 +1,25 @@
+<template lang="">
+    <div>
+        login
+    </div>
+</template>
+<script lang="js">
+// pages/login.vue
+
+const client = useSupabaseAuthClient();
+const router = useRouter();
+const user = useSupabaseUser();
+
+async function login() {
+  const { email, password } = credentials;
+  const { error } = await client.auth.signInWithPassword({ email, password });
+  if (!error) return router.push('/');
+  console.log(error);
+}
+
+watchEffect(async () => {
+  if (user.value) {
+    await router.push('/');
+  }
+});
+</script>
